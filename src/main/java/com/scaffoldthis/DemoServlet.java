292ac3e05e57b792d21561b556d18eb56bdb522f
Nitish Kumar <nitishkumar01@gmail.com>
2016-08-07 23:31:30 +0530

    jsp completed code

--- src/main/java/com/scaffoldthis/DemoServlet.java
+++ src/main/java/com/scaffoldthis/DemoServlet.java
@@ -8,34 +8,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class DemoServlet extends HttpServlet {
-
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String html = "<html>\n" + 
-				"	<head>\n" + 
-				"		\n" + 
-				"	</head>\n" + 
-				"	<body>\n" + 
-				"		<form action=\"/hello\" method=\"post\">\n" + 
-				"			<input type=\"text\" placeholder=\"Username\" name=\"username\"><br/>\n" + 
-				"			<input type=\"password\" placeholder=\"Password\" name=\"password\"><br/>\n" + 
-				"			<input type=\"submit\" value=\"login\">\n" + 
-				"		</form>\n" + 
-				"	</body>\n" + 
-				"</html>";
-		resp.getWriter().println(html);
-	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String username = req.getParameter("username");
 		String password = req.getParameter("password");
 		if(username.equals("admin") && password.equals("admin")){
-			resp.getWriter().println("Welcome Admin");
+			req.setAttribute("username", "admin");
+			req.getRequestDispatcher("home.jsp").forward(req, resp);
 		}
 		else
 		{
-			resp.getWriter().println("Invalid login");
+			resp.sendRedirect("login.jsp");
 		}
 	}
 	
